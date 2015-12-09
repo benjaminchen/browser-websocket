70ff1327df28b9f1c5dd2676c97e5961cd616434
benjamin <benjaminchen.c@gmail.com>
2015-12-09 14:05:29 +0800

    change variable scope

--- libs/BrowserWebSocket.js
+++ libs/BrowserWebSocket.js
@@ -1,55 +1,52 @@
-var ws, debugMode;
-
 function BrowserWebSocket(url, debug) {
     if (typeof WebSocket == 'undefined') return false;
     if (this === (function(){return this;})()) return new BrowserWebSocket(url, debug);
 
     this.url = url;
-    ws = new WebSocket(url);
-    debugMode = debug;
+    this.ws = new WebSocket(url);
+    this.debugMode = debug;
 }
 
-BrowserWebSocket.prototype.onopen = function(callback) {
-    var url = this.url;
-    ws.addEventListener('open', function() {
-        if (debugMode) console.log("connect [" + url + "] successful ...");
-        if (callback) callback();
-    });
-};
-
-BrowserWebSocket.prototype.onclose = function(callback) {
-    ws.addEventListener('close', function() {
-        if (debugMode) console.log("connection already interrupted ...");
-        if (callback) callback();
-    });
-};
-
-BrowserWebSocket.prototype.onerror = function(callback) {
-    ws.addEventListener('error', function(e) {
-        if (debugMode) console.log("something error ...");
-        if (callback) callback(e.data);
-    });
-};
-
-BrowserWebSocket.prototype.onmessage = function(callback) {
-    ws.addEventListener('message', function(e) {
-        if (debugMode) console.log("received message [" + e.data + "] form server ...");
-        if (callback) callback(e.data);
-    });
-};
-
-BrowserWebSocket.prototype.emit = function(message) {
-    if (debugMode) console.log("send message [" + message + "] to server ...");
-    ws.send(message);
-};
-
-BrowserWebSocket.prototype.close = function() {
-    if (debugMode) console.log("close connection ...");
-    ws.close();
-};
-
-BrowserWebSocket.prototype.off = function(event, fn) {
-    ws.removeEventListener(event, fn);
+BrowserWebSocket.prototype = {
+    onopen: function(callback) {
+        var me = this;
+        me.ws.addEventListener('open', function() {
+            if (me.debugMode) console.log("connect [" + me.url + "] successful ...");
+            if (callback) callback();
+        });
+    },
+    onclose: function(callback) {
+        var me = this;
+        me.ws.addEventListener('close', function() {
+            if (me.debugMode) console.log("connection already interrupted ...");
+            if (callback) callback();
+        });
+    },
+    onerror: function(callback) {
+        var me = this;
+        me.ws.addEventListener('error', function(e) {
+            if (me.debugMode) console.log("something error ...");
+            if (callback) callback(e.data);
+        });
+    },
+    onmessage: function(callback) {
+        var me = this;
+        me.ws.addEventListener('message', function(e) {
+            if (me.debugMode) console.log("received message [" + e.data + "] form server ...");
+            if (callback) callback(e.data);
+        });
+    },
+    emit: function(message) {
+        if (this.debugMode) console.log("send message [" + message + "] to server ...");
+        this.ws.send(message);
+    },
+    close: function() {
+        if (this.debugMode) console.log("close connection ...");
+        this.ws.close();
+    },
+    off: function(event, fn) {
+        this.ws.removeEventListener(event, fn);
+    }
 };
 
 module.exports = BrowserWebSocket;
